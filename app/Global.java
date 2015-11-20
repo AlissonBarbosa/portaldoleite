405105a95e13ef4d3b0655dbdfe68033e481051a
AlissonBarbosa <alisson.89.sud@gmail.com>
2015-11-20 11:05:09 -0300

    Adicionando novas disciplinas e um usuário teste

--- app/Global.java
+++ app/Global.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
+import models.User;
 import models.Disciplina;
 import models.Tema;
 import models.dao.GenericDAOImpl;
@@ -25,6 +26,7 @@ public class Global extends GlobalSettings {
 				if(dao.findAllByClassName(Disciplina.class.getName()).size() == 0){
 					criaDisciplinaTemas();
 				}
+				addUsuario();
 			}
 		});
 	}
@@ -58,20 +60,38 @@ public class Global extends GlobalSettings {
 		si1.addTema(new Tema("Minitestes"));
 		si1.addTema(new Tema("Projeto"));
 		dao.persist(si1);
-		Disciplina si2 = new Disciplina("Sistemas de Informação 2");
-		si2.addTema(new Tema("Design"));
-		si2.addTema(new Tema("OO"));
-		si2.addTema(new Tema("GRASP 1"));
-		si2.addTema(new Tema("GoF 1"));
-		si2.addTema(new Tema("Arquiteturas"));
-		si2.addTema(new Tema("Play Framework"));
-		si2.addTema(new Tema("JavaScript Introdução"));
-		si2.addTema(new Tema("HTML / CSS / Bootstrap básico"));
-		si2.addTema(new Tema("Heroku introdução"));
-		si2.addTema(new Tema("Labs"));
-		si2.addTema(new Tema("Minitestes"));
-		si2.addTema(new Tema("Projeto"));
-		dao.persist(si2);
+		Disciplina eda = new Disciplina("Estrutuda de Dados");
+		eda.addTema(new Tema("Notação Assintótica"));
+		eda.addTema(new Tema("Análise de Algoritmos Recursivos"));
+		eda.addTema(new Tema("Ordenação por comparação"));
+		eda.addTema(new Tema("Ordenação por tempo linear"));
+		eda.addTema(new Tema("TAD"));
+		eda.addTema(new Tema("Listas encadeadas"));
+		eda.addTema(new Tema("ABP"));
+		eda.addTema(new Tema("HEAPS"));
+		eda.addTema(new Tema("Skip List"));
+		eda.addTema(new Tema("Tabela Hash"));
+		eda.addTema(new Tema("AVL"));
+		eda.addTema(new Tema("Árvores"));
+		dao.persist(eda);
+		Disciplina p2 = new Disciplina("Programação II");
+		p2.addTema(new Tema("Introdução a Java"));
+		p2.addTema(new Tema("Introdução a OO"));
+		p2.addTema(new Tema("Criação de Classes"));
+		p2.addTema(new Tema("Testes de unidade"));
+		p2.addTema(new Tema("Coleções"));
+		p2.addTema(new Tema("Reuso, Composição e Herança"));
+		p2.addTema(new Tema("Herança e polimorfismo"));
+		p2.addTema(new Tema("Interfaces e polimorfismo"));
+		p2.addTema(new Tema("Tratamento de erros e exceções"));
+		p2.addTema(new Tema("Recursividade"));
+		dao.persist(p2);
 		dao.flush();
+	}
+
+	private void addUsuario(){
+		User usuario1 = new User("antonio@nsa.com", "antonio123", "antonio");
+		usuario1.setNome("Antonio");
+		dao.persist(usuario1);
 	}
 }
