558051c49aee9d59b8dad6d257ed3b1b3eac7e7a
Mateus <mateuslunacc@gmail.com>
2015-11-25 18:36:29 -0300

    Adicionei votos as dicas dos usuarios.

--- app/Global.java
+++ app/Global.java
@@ -27,7 +27,7 @@ public class Global extends GlobalSettings {
 
 	@Override
 	public void onStart(Application app) {
-		Logger.info("AplicaÃ§Ã£o inicializada...");
+		Logger.info("Aplicação inicializada...");
 
 		JPA.withTransaction(new play.libs.F.Callback0() {
 			@Override
@@ -46,7 +46,7 @@ public class Global extends GlobalSettings {
 	    JPA.withTransaction(new play.libs.F.Callback0() {
 	    @Override
 	    public void invoke() throws Throwable {
-	        Logger.info("AplicaÃ§Ã£o finalizando...");
+	        Logger.info("Aplicação finalizando...");
 	        disciplinas = dao.findAllByClassName("Disciplina");
 
 	        for (Disciplina disciplina: disciplinas) {
@@ -56,9 +56,7 @@ public class Global extends GlobalSettings {
 	}
 	
 	private void criaDisciplinaTemas(){
-		Disciplina si1 = new Disciplina("Sistemas de InformaÃ§Ã£o 1");
-		si1.addTema(new Tema("AnÃ¡lise x Design"));
-		si1.addTema(new Tema("OrientaÃ§Ã£o a objetos"));
+		Disciplina si1 = new Disciplina("Sistemas de Informação 1");
 		si1.addTema(new Tema("Análise x Design"));
 		si1.addTema(new Tema("Orientação a objetos"));
 		si1.addTema(new Tema("GRASP"));
@@ -71,15 +69,9 @@ public class Global extends GlobalSettings {
 		si1.addTema(new Tema("Labs"));
 		si1.addTema(new Tema("Minitestes"));
 		si1.addTema(new Tema("Projeto"));
-		
-		si1.getTemaByNome("Labs").addDica(new DicaConselho("Melhor começar o quanto antes."));
 		dao.persist(si1);
 
 		Disciplina eda = new Disciplina("Estrutuda de Dados");
-		eda.addTema(new Tema("NotaÃ§Ã£o AssintÃ³tica"));
-		eda.addTema(new Tema("AnÃ¡lise de Algoritmos Recursivos"));
-		eda.addTema(new Tema("OrdenaÃ§Ã£o por comparaÃ§Ã£o"));
-		eda.addTema(new Tema("OrdenaÃ§Ã£o por tempo linear"));
 		eda.addTema(new Tema("Notação Assintótica"));
 		eda.addTema(new Tema("Análise de Algoritmos Recursivos"));
 		eda.addTema(new Tema("Ordenação por comparação"));
@@ -91,31 +83,20 @@ public class Global extends GlobalSettings {
 		eda.addTema(new Tema("Skip List"));
 		eda.addTema(new Tema("Tabela Hash"));
 		eda.addTema(new Tema("AVL"));
-		eda.addTema(new Tema("Ã�rvores"));
-
-		eda.getTemaByNome("HEAPS").addDica(new DicaMaterial("http://pages.cs.wisc.edu/~paton/readings/Old/fall01/HEAP-SORT.htm"));
-		eda.getTemaByNome("Skip List").addDica(new DicaMaterial("https://www.quora.com/What-uses-are-there-for-Skip-Lists"));
-
+		eda.addTema(new Tema("Árvores"));
 		dao.persist(eda);
 
-		Disciplina p2 = new Disciplina("ProgramaÃ§Ã£o II");
-		p2.addTema(new Tema("IntroduÃ§Ã£o a Java"));
-		p2.addTema(new Tema("IntroduÃ§Ã£o a OO"));
-		p2.addTema(new Tema("CriaÃ§Ã£o de Classes"));
+		Disciplina p2 = new Disciplina("Programação II");
 		p2.addTema(new Tema("Introdução a Java"));
 		p2.addTema(new Tema("Introdução a OO"));
 		p2.addTema(new Tema("Criação de Classes"));
 		p2.addTema(new Tema("Testes de unidade"));
-		p2.addTema(new Tema("ColeÃ§Ãµes"));
-		p2.addTema(new Tema("Reuso, ComposiÃ§Ã£o e HeranÃ§a"));
-		p2.addTema(new Tema("HeranÃ§a e polimorfismo"));
+		p2.addTema(new Tema("Coleções"));
+		p2.addTema(new Tema("Reuso, Composição e Herança"));
+		p2.addTema(new Tema("Herança e polimorfismo"));
 		p2.addTema(new Tema("Interfaces e polimorfismo"));
-		p2.addTema(new Tema("Tratamento de erros e exceÃ§Ãµes"));
+		p2.addTema(new Tema("Tratamento de erros e exceções"));
 		p2.addTema(new Tema("Recursividade"));
-		
-		p2.getTemaByNome("HeranÃ§a e polimorfismo").addDica(new DicaAssunto("É útil para padrões de projeto."));
-		p2.getTemaByNome("Testes de unidade").addDica(new DicaConselho("Importante para os seus colegas entenderem a funcionalidade do seu código."));
-		
 		dao.persist(p2);
 		
 		dao.flush();
@@ -165,16 +146,43 @@ public class Global extends GlobalSettings {
 
 	private void addDica(){
 		// Dicas para SI
-		//Dica dica1 = new DicaConselho("Faça todos os Labs");
-		//dica1.setTema(si1.getTemaByNome("Labs"));
-		//dica1.setUser("Antonio");
-		//dao.persist(dica1);
+		Dica dica1 = new DicaConselho("Melhor começar o quanto antes.");
+		dica1.setTema(si1.getTemaByNome("Labs"));
+		dica1.setUser("Antonio");
+		dica1.setConcordancias(1);
+		dica1.setDiscordancias(1);
+		dao.persist(dica1);
 
 		// Dicas para EDA
-		//Dica dica2 = new DicaDisciplina("P2","É muito importante que aprenda o máximo de JAVA nessa disciplina");
-		//dica2.setTema(eda.getTemaByNome("Ordenação por comparação"));
-		//dica2.setUser("Hulk");
-		//dao.persist(dica2);
+		Dica dica2 = new DicaMaterial("http://pages.cs.wisc.edu/~paton/readings/Old/fall01/HEAP-SORT.htm");
+		dica2.setTema(eda.getTemaByNome("HEAPS"));
+		dica2.setUser("Hulk");
+		dica2.setConcordancias(1);
+		dica2.setDiscordancias(2);
+		dao.persist(dica2);
+		
+		Dica dica3 = new DicaMaterial("https://www.quora.com/What-uses-are-there-for-Skip-Lists");
+		dica3.setTema(eda.getTemaByNome("Skip List"));
+		dica3.setUser("Fenix");
+		dica3.setConcordancias(5);
+		dica3.setDiscordancias(2);
+		dao.persist(dica3);
+		
+		// Dicas para P2
+		Dica dica4 = new DicaAssunto("É útil para padrões de projeto.");
+		dica4.setTema(p2.getTemaByNome("Herança e polimorfismo"));
+		dica4.setUser("Mariana");
+		dica4.setConcordancias(2);
+		dica4.setDiscordancias(0);
+		dao.persist(dica4);
+
+		Dica dica5 = new DicaConselho("https://www.quora.com/What-uses-are-there-for-Skip-Lists");
+		dica5.setTema(p2.getTemaByNome("Testes de unidade"));
+		dica5.setUser("The Penguin");
+		dica5.setConcordancias(2);
+		dica5.setDiscordancias(4);
+		dao.persist(dica5);
 
 	}
+	
 }
